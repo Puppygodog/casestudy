a3a992966cb335682f3efcd85a45ef56e0c43c59
Puppygodog <71526466+Puppygodog@users.noreply.github.com>
2022-06-28 10:50:43 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+    backpack = loadImage('bluebackpack.png');
+}
+
+function setup(){
+    canvas = createCanvas(350,300);
+    canvas.center();
+    //canvas.image = "bluebackpack.png";
+    
+}
+
+
+function draw(){
+}
+
